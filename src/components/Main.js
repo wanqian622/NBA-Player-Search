7685fe9374c73cca9c8fcc0cc06582d8def51c56
wanqian622 <wanqian622@gmail.com>
2018-04-07 21:00:29 -0700

    finish short chat

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {ShotChart} from "./ShotChart"
+
+export class Main extends React.Component{
+    render(){
+        return(
+            <div >
+                <ShotChart playerId={2544}/>
+            </div>
+
+        )
+    }
+}
