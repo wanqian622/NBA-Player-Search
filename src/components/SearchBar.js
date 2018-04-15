163593d03797b485ef53e70c1c4a683705582977
wanqian622 <wanqian622@gmail.com>
2018-04-15 10:46:55 -0700

    show image in search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,9 @@
 import { AutoComplete, Input, Icon} from 'antd';
 import React from 'react';
 import nba from 'nba';
+import {PROFILE_PIC_URL_PREFIX} from "../constants"
+
+const Option = AutoComplete.Option;
 
 
 
@@ -15,7 +18,10 @@ export class SearchBar extends React.Component {
 
     handleSearch = (value) => {
         this.setState({
-            dataSource: !value ? [] : nba.searchPlayers(value).map(player=>player.fullName)
+            dataSource: !value ? [] : nba.searchPlayers(value).map(({fullName, playerId})=><Option key = {playerId} value = {fullName}>
+                <img src = {`${PROFILE_PIC_URL_PREFIX}/${playerId}.png`} />
+                <span>{fullName}</span>
+            </Option>)
         });
     }
 
@@ -31,6 +37,7 @@ export class SearchBar extends React.Component {
                 onSelect={this.onSelect}
                 onSearch={this.handleSearch}
                 placeholder="Search NBA Player"
+                optionLabelProp="value"
             >
                 <Input suffix={<Icon type="search" className="certain-category-icon" />} />
             </AutoComplete>
